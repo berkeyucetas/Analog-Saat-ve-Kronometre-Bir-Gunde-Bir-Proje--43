21cd6ebbbc8837e9583eb1cad5d11ed7b69d6b49
Berke <116177776+berkeyucetas@users.noreply.github.com>
2023-11-13 16:27:44 +0300

    Analog-Saat-ve-Kronometre-Bir-Gunde-Bir-Proje--43
    Bugün saat ve kronometre projesi yaptım .

---
+++ Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace Analog_Saat_ve_Kronometre
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.btnDuraklat = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.labelSalise = new System.Windows.Forms.Label();
+            this.labelSaniye = new System.Windows.Forms.Label();
+            this.labelDakika = new System.Windows.Forms.Label();
+            this.labelSaat = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(515, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(533, 398);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(138, 40);
+            this.btnBasla.TabIndex = 1;
+            this.btnBasla.Text = "Start\r\n";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // btnDuraklat
+            // 
+            this.btnDuraklat.Location = new System.Drawing.Point(677, 398);
+            this.btnDuraklat.Name = "btnDuraklat";
+            this.btnDuraklat.Size = new System.Drawing.Size(138, 40);
+            this.btnDuraklat.TabIndex = 2;
+            this.btnDuraklat.Text = "Stop";
+            this.btnDuraklat.UseVisualStyleBackColor = true;
+            this.btnDuraklat.Click += new System.EventHandler(this.btnDuraklat_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(821, 398);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(138, 40);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(559, 207);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Saat";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(608, 207);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Dakika";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(674, 207);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Saniye";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(739, 207);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Salise";
+            // 
+            // labelSalise
+            // 
+            this.labelSalise.AutoSize = true;
+            this.labelSalise.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSalise.Location = new System.Drawing.Point(757, 232);
+            this.labelSalise.Name = "labelSalise";
+            this.labelSalise.Size = new System.Drawing.Size(18, 20);
+            this.labelSalise.TabIndex = 11;
+            this.labelSalise.Text = "0";
+            // 
+            // labelSaniye
+            // 
+            this.labelSaniye.AutoSize = true;
+            this.labelSaniye.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSaniye.Location = new System.Drawing.Point(694, 232);
+            this.labelSaniye.Name = "labelSaniye";
+            this.labelSaniye.Size = new System.Drawing.Size(18, 20);
+            this.labelSaniye.TabIndex = 10;
+            this.labelSaniye.Text = "0";
+            // 
+            // labelDakika
+            // 
+            this.labelDakika.AutoSize = true;
+            this.labelDakika.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelDakika.Location = new System.Drawing.Point(629, 232);
+            this.labelDakika.Name = "labelDakika";
+            this.labelDakika.Size = new System.Drawing.Size(18, 20);
+            this.labelDakika.TabIndex = 9;
+            this.labelDakika.Text = "0";
+            // 
+            // labelSaat
+            // 
+            this.labelSaat.AutoSize = true;
+            this.labelSaat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.labelSaat.Location = new System.Drawing.Point(571, 232);
+            this.labelSaat.Name = "labelSaat";
+            this.labelSaat.Size = new System.Drawing.Size(18, 20);
+            this.labelSaat.TabIndex = 8;
+            this.labelSaat.Text = "0";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(821, 180);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(138, 212);
+            this.listBox1.TabIndex = 12;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.labelSalise);
+            this.Controls.Add(this.labelSaniye);
+            this.Controls.Add(this.labelDakika);
+            this.Controls.Add(this.labelSaat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnDuraklat);
+            this.Controls.Add(this.btnBasla);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Button btnDuraklat;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label labelSalise;
+        private System.Windows.Forms.Label labelSaniye;
+        private System.Windows.Forms.Label labelDakika;
+        private System.Windows.Forms.Label labelSaat;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
